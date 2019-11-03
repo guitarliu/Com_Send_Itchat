3ff38691a25f1317a495fcb62bb5cb85c1996a9f
guitarliu <whuliuxiaoyong@163.com>
2019-11-03 13:22:20 +0800

    add voice function

--- get_message.py
+++ get_message.py
@@ -1,13 +1,17 @@
 # coding: utf-8
 
-import itchat, time
+import itchat, time, win32com
 from itchat.content import *
+from win32com.client import Dispatch
 
 newInstance = itchat.new_instance()
 newInstance.auto_login(hotReload=True, enableCmdQR=True, statusStorageDir='newInstance.pkl')
+speaker = Dispatch("SAPI.SpVoice")
+
 
 @newInstance.msg_register(TEXT)
 def reply(msg):
         print(msg.user['RemarkName'], ": ", msg.text)
-
+        speaker.Speak("Message From" + msg.user['RemarkName'] + msg.text)
+        
 newInstance.run()
